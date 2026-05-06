7304bfa821644aa537b2a8bf69989f68de8abe08
mplorentz <mplorentz@users.noreply.github.com>
2026-05-06 11:41:45 -0400

    Reapply "Merge pull request #127 from mplorentz/cursor/279e398c"
    This reverts commit dac6dc31832e82ffca6757c6f0c26e263bfd5c27.

---
+++ lib/database/daos/steward_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'steward_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$StewardDaoMixin on DatabaseAccessor<AppDatabase> {
+  $VaultsTable get vaults => attachedDatabase.vaults;
+  $StewardsTable get stewards => attachedDatabase.stewards;
+}
